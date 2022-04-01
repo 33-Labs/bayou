5f1515929687e9e4c0113398c623caaff96fb4b1
lanford <lanfordcai@outlook.com>
2022-04-02 01:35:01 +0800

    feat: speed up image loading

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -5,17 +5,17 @@ export default function NavigationBar() {
   return (
     <>
       <div className="relative gap-x-2 flex items-center bg-white h-48">
-        <Image src="/bayou.png" alt="" width={50} height={50} />
+        <Image src="/bayou.png" alt="" width={50} height={50} priority />
         <label className="font-flow font-bold text-4xl">
           Bayou
         </label>
         <label className="font-flow text-flow-green border border-flow-green text-sm whitespace-pre"> {publicConfig.chainEnv} </label>
         <a 
-          href={publicConfig.flowscanURL}
+          href={"https://github.com/33-Labs/bayou"}
           target="_blank" 
           rel="noopener noreferrer"
-          className="absolute right-0 font-flow text-lg underline decoration-flow-green decoration-2">
-            Flowscan
+          className="absolute right-0">
+            <Image src="/github.png" alt="" width={24} height={24} priority />
         </a>
       </div>
     </>
