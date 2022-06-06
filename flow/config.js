92139777511f2d32471a8157ce7dfdee9088fa8e
lanford <lanfordcai@outlook.com>
2022-06-06 17:07:25 +0800

    feat: support lilico

--- flow/config.js
+++ flow/config.js
@@ -1,7 +1,11 @@
 import { config } from "@onflow/fcl";
 import publicConfig from "../publicConfig";
+import {send as httpSend} from "@onflow/transport-http";
 
 config({
   "accessNode.api": publicConfig.accessNodeAPI,
   "discovery.wallet": publicConfig.walletDiscovery,
+  "sdk.transport": httpSend,
+  "app.detail.title": "Bayou",
+  "app.detail.icon": "https://bayou.vercel.app/_next/image?url=%2Fbayou.png&w=128&q=100"
 })
