c6f54ab69d136a1623fb7f1419969ec4edabe6e6
lanford33 <lanfordcai@outlook.com>
2022-10-21 22:12:32 +0800

    feat: update home page

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -4,19 +4,28 @@ import publicConfig from "../publicConfig"
 export default function NavigationBar() {
   return (
     <>
-      <div className="relative gap-x-2 flex items-center h-44">
-        <Image src="/bayou.png" alt="" width={50} height={50} priority />
-        <label className="font-flow font-bold text-3xl">
-          bayou
-        </label>
-        <label className="font-flow text-flow-green border border-flow-green text-sm whitespace-pre"> {publicConfig.chainEnv} </label>
-        <a 
-          href={"https://github.com/33-Labs/bayou"}
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="absolute right-0">
-            <Image src="/github.png" alt="" width={24} height={24} priority />
-        </a>
+      <div className="gap-x-2 flex items-center justify-between h-44">
+        <div className="relative gap-x-2 flex items-center">
+          <Image src="/bayou.png" alt="" width={50} height={50} priority />
+          <label className="font-flow font-bold text-3xl">
+            bayou
+          </label>
+          <label className="font-flow text-flow-green border border-flow-green text-sm whitespace-pre"> {publicConfig.chainEnv} </label>
+        </div>
+        <div className="flex gap-x-2 items-center">
+          <div className="relative w-[25px] h-[25px] rounded-full">
+            <Image src="/flow.png" alt="" objectPosition="50% 50%" objectFit="contain" layout="fill" priority />
+          </div>
+          <div className="relative w-[22px] h-[22px]">
+            <a
+              href={"https://aptos.bayou33.app"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="">
+              <Image src="/aptos.svg" alt="" objectPosition="50% 50%" objectFit="contain" layout="fill" priority />
+            </a>
+          </div>
+        </div>
       </div>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home(props) {
         <title>bayou | batch token transfer tool</title>
         <meta property="og:title" content="bayou | batch token transfer tool" key="title" />
       </Head>
-      <div className="container mx-auto max-w-[680px] min-w-[350px] px-8">
+      <div className="container mx-auto max-w-[880px] min-w-[350px] px-8">
         <NavigationBar user={user} />
         <WalletConnector
           className="mt-12 w-full"
